d619d4044339d48ce746f631e54eeec0f9eac856
Phil <224063001@qq.com>
2017-10-17 17:59:15 +0800

    add createPresentableModule

--- client/src/views/node/contracts/list/index.js
+++ client/src/views/node/contracts/list/index.js
@@ -1,4 +1,5 @@
 import TableView from '@/components/TableView/index.vue'
+import contract from "../../../../services/contract";
 
 export default {
   name: 'node-contracts',
@@ -24,34 +25,85 @@ export default {
     queryHandler() {
       this.$message.warning('待开发')
     },
+    loadContracts(param) {
+      return this.$services.contract.get(param || {}).then((res) => {
+        var data = res.data;
+
+        if (data.ret === 0) {
+          return res
+        } else {
+          Promise.reject(new Error(data.msg))
+        }
+      })
+    },
+    loadPresentables(param) {
+      return this.$services.presentables.get({params: param}).then((res) => {
+        var data = res.data;
+
+        if (data.ret === 0) {
+          return res
+        } else {
+          Promise.reject(new Error(data.msg))
+        }
+      })
+    },
+    mergeData(contracts, presentables) {
+      var presentableMap = {}
+      presentables.forEach((p) => {
+        presentableMap[p.contractId] = p
+      })
+      contracts.forEach((contract) => {
+        Object.assign(contract, {
+          presentableDetail: presentableMap[contract.contractId] || null
+        })
+      })
+    },
     loader() {
+      const self = this;
       return (param) => {
+        const nodeId = self.$route.params.nodeId
         if (typeof param === 'object') {
           Object.assign(param, {
-            partyTwo: this.$route.params.nodeId
+            partyTwo: nodeId
           })
           param = {
             params: param
           }
         }
-
-        return this.$services.contract.get(param || {})
-      }
-    },
-    handlePresentable(row) {
-      if (!row.contractId) {
-        this.$message.warning('无效合同，无法进行创建presentable');
-      } else {
-        this.$router.push({
-          path: `/node/${this.$route.params.nodeId}/presentable/create`,
-          query: {contractId: row.contractId}
+        return self.loadContracts(param).then((res) => {
+          var contracts = res.data.data.dataList
+          var contractIds = contracts.map((c) => {
+            return c.contractId
+          })
+          return self.loadPresentables({
+            contractIds: contractIds.join(','),
+            nodeId: nodeId
+          }).then((res2) => {
+            var presentables = res2.data.data;
+            self.mergeData(contracts, presentables)
+            return res
+          })
         })
       }
     },
-    previewHandler(resource) {
+    handlePresentable(row) {
+      var nodeId = this.$route.params.nodeId
+      if (!row.presentableDetail) {
+        this.$router.push({
+          path: `/node/${nodeId}/presentable/create`,
+          query: {contractId: row.contractId}
+        })
+      } else {
+        this.$router.push({
+          path: `/node/${nodeId}/presentable/detail`,
+          query: {presentableId: row.presentableDetail.presentableId}
+        })
+      }
+    },
+    previewHandler(row) {
       this.$router.push({
         path: `/node/${this.$route.params.nodeId}/contract/detail`,
-        query: {contractId: resource.contractId}
+        query: {contractId: row.contractId}
       })
     }
   }
--- client/src/views/node/presentable/detail/index.vue
+++ client/src/views/node/presentable/detail/index.vue
@@ -1,14 +1,17 @@
 <template>
   <section>
-    <el-form :model="detail"class="node-detail" ref="detail" label-width="100px">
+    <el-form :model="detail" ref="detail" label-width="100px">
+      <el-form-item label="name" prop="name">
+        <el-input v-model="detail.name"></el-input>
+      </el-form-item>
       <el-form-item label="contractId" prop="contractId">
         <el-input v-model="detail.contractId" disabled></el-input>
       </el-form-item>
-      <el-form-item label="status" prop="status">
-        <el-input v-model="detail.status" disabled></el-input>
+      <el-form-item label="presentableId" prop="presentableId">
+        <el-input v-model="detail.presentableId" disabled></el-input>
       </el-form-item>
       <el-form-item class="btns">
-        <el-button type="primary" @click="updateNodeDetail('detail')">保存</el-button>
+        <el-button type="primary" @click="updateDetail('detail')">保存</el-button>
       </el-form-item>
     </el-form>
   </section>
@@ -16,19 +19,9 @@
 
 <script>
   import PresentableDetail from './index'
-
   export default PresentableDetail
 </script>
 
 <style lang="less" scoped>
-  .resource-detail {
-    width: 80%;
-    margin: auto;
-    .btns {
-      text-align: center;
-    }
-  }
-.meta-detail-item {
-  margin-bottom: 9px;
-}
+
 </style>
