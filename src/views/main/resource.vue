3878cf29427df971134b6c56c9552209a7b9ea81
daizch <daizech@gmail.com>
2018-10-31 17:32:27 +0800

    修改主页资源列表UI

---
+++ src/views/main/resource.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="main-resource-item-wrap" @click="gotoDetail">
+    <div class="res-poster-wrap">
+      <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540792719445&di=124be59785895fe7872df620be3bde7d&imgtype=0&src=http%3A%2F%2Fwww.tupperware.com.cn%2Ffileserver%2Fproduct%2F201407%2F2014-7-24_15-14-16_979_401_b.jpg"
+           alt="">
+    </div>
+    <div class="res-digest-info">
+      <p class="res-name">{{resource.resourceName}}</p>
+      <p class="res-author-name" v-if="resource._userInfo"><span>{{resource.resourceType}}</span>{{resource._userInfo.nickname}}</p>
+    </div>
+    <ul class="res-policies-list" v-if="schemes.length">
+      <li class="res-p-item" :class="resSchemeStatusCls(scheme)" v-for="scheme in schemes">
+        {{scheme.authSchemeName[0]}}
+        <div class="res-scheme-info-wrap">
+          <h4>免费授权方案</h4>
+          <div class="res-scheme-item">
+            <p>授权对象：</p>
+            <ul class="res-scheme-item res-scheme-auth-targets">
+              <li v-for="user in scheme.targets">{{user}}</li>
+            </ul>
+          </div>
+          <div class="res-scheme-item">
+            <p>上抛：</p>
+            <div><i class="res-scheme-status-icon"></i>{{scheme.isBubbled? '有': '无'}}资源上抛</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import {onloadUserInfo} from '@/data/user/loader'
+  import {onloadSchemesForResource} from '@/data/scheme/loader'
+  import {RESOURCE_STATUS} from '@/config/resource'
+
+  export default {
+    name: 'main-list-resource-item',
+
+    data() {
+      return {
+        schemes: []
+      }
+    },
+    props: {
+      resource: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      type: {
+        type: String,
+        default: 'list'
+      }
+    },
+
+    mounted() {
+      this.format(this.resource)
+    },
+
+    methods: {
+      resSchemeStatusCls(scheme){
+        return scheme.bubbleResources.length? 'status-1' : 'status-0'
+      },
+      format(resource) {
+        if (!this.resource.resourceId) {
+          return
+        }
+
+        resource._statusInfo = RESOURCE_STATUS[resource.status]
+        onloadUserInfo(resource.userId).then((userInfo) => {
+          this.$set(resource, '_userInfo', userInfo)
+        })
+
+        onloadSchemesForResource(resource.resourceId).then(list => {
+          this.schemes = list.map(this.resolveScheme)
+          console.log(list)
+        })
+      },
+      resolveScheme(scheme){
+        var users = new Set()
+        scheme.policy.forEach(p=>{
+          p.authorizedObjects.forEach(objs=>{
+            objs.users.forEach(u=>{
+              users.add(u)
+            })
+          })
+        })
+
+        scheme.targets = Array.from(users)
+        scheme.isBubbled = scheme.bubbleResources.length > 0
+        return scheme
+      },
+      gotoDetail() {
+        this.$router.push(`/resource/detail/${this.resource.resourceId}`)
+      }
+    }
+  }
+</script>
+
+
+<style lang="less" type="text/less" scoped>
+  @import "../../styles/mixin";
+  @status0Color: #009AFF;
+  @status1Color: #EDA02C;
+  .main-resource-item-wrap {
+    overflow: hidden;
+    cursor: pointer;
+    position: relative;
+
+    .res-scheme-info-wrap {
+      position: absolute;
+      bottom: 40px;
+      top: 0;
+      right: 0;
+      left: 100%;
+      padding: 20px 10px 0;
+      background-color: white;
+      color: #222222;
+      font-size: 14px;
+      transition: left .3s;
+      text-align: left;
+      width: 100%;
+
+      h4 {
+        font-size: 16px;
+        margin-bottom: 25px;
+      }
+
+      .res-scheme-item {
+        margin-bottom: 25px;
+        p {
+          margin-bottom: 10px;
+        }
+
+        i {
+          border: 5px solid;
+          border-radius: 50%;
+          height: 0;
+          width: 0;
+          display: inline-block;
+          margin-right: 5px;
+        }
+      }
+      .res-scheme-auth-targets {
+        display: flex;
+        li {
+          color: @status0Color;
+          border: 1px solid @status0Color;
+          border-radius: 11px;
+          padding: 0 10px;
+          margin-right: 10px;
+        }
+      }
+    }
+
+    .res-policies-list {
+      display: flex;
+      align-items: center;
+      padding-left: 10px;
+      .res-p-item {
+        margin-right: 6px;
+        border: 1px solid;
+        border-radius: 11px;
+        padding: 0 6px;
+        text-align: center;
+
+        /*无上抛*/
+        &.status-0 {
+          border-color: @status0Color;
+          color: @status0Color;
+
+          .res-scheme-status-icon {
+            border-color: @status0Color;
+          }
+        }
+
+        /*有上抛*/
+        &.status-1 {
+          border-color: @status1Color;
+          color: @status1Color;
+          .res-scheme-status-icon {
+            border-color: @status1Color;
+          }
+        }
+
+        &:hover {
+          color: white;
+          .res-scheme-info-wrap {
+            left: 0
+          }
+
+          &.status-0 {
+            background-color: @status0Color;
+          }
+
+          &.status-1 {
+            background-color: @status1Color;
+          }
+        }
+      }
+    }
+
+    img {
+      width: 100%;
+      height: 165px;
+    }
+    .res-digest-info {
+      padding: 10px 10px 8px;
+      p {
+        padding-bottom: 10px;
+        color: #333333;
+        .text-ellipsis;
+      }
+
+      p:last-child {
+        border-bottom: 1px solid #E6E6E6;
+      }
+
+      .res-author-name {
+        span {
+          float: right;
+          margin-left: 50px;
+        }
+      }
+
+      .res-name {
+        color: #222222;
+        font-weight: 500;
+        font-size: 14px;
+      }
+    }
+
+  }
+</style>
