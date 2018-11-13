9110e667a89082de4c2bc9933a500d36e0f49d34
daizch <daizech@gmail.com>
2018-11-13 11:35:02 +0800

    fix codemirror

--- src/lib/codemirror.js
+++ src/lib/codemirror.js
@@ -1,22 +1,22 @@
 import { codemirror } from 'vue-codemirror'
 
-require('codemirror/lib/codemirror.css')
+import 'codemirror/lib/codemirror.css'
 
-require('codemirror/theme/dracula.css')
-require('codemirror/addon/fold/foldgutter.css')
-require('codemirror/addon/fold/foldgutter')
-require('codemirror/addon/fold/foldcode')
-require('codemirror/addon/fold/xml-fold')
-require('codemirror/addon/fold/brace-fold')
-require('codemirror/addon/fold/indent-fold')
-require('codemirror/addon/edit/matchbrackets')
-require('codemirror/addon/selection/selection-pointer')
+import 'codemirror/theme/dracula.css'
+import 'codemirror/addon/fold/foldgutter.css'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/addon/fold/foldcode'
+import 'codemirror/addon/fold/xml-fold'
+import 'codemirror/addon/fold/brace-fold'
+import 'codemirror/addon/fold/indent-fold'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/addon/selection/selection-pointer'
 
-require('codemirror/keymap/sublime')
-require('codemirror/addon/search/match-highlighter')
-require('codemirror/addon/search/searchcursor')
-require('codemirror/addon/display/placeholder')
+import 'codemirror/addon/search/match-highlighter'
+import 'codemirror/addon/search/searchcursor'
+import 'codemirror/addon/display/placeholder'
 
+// import 'codemirror/keymap/sublime'
 
 export const codeMirrorOptions = {
   tabSize: 4,
@@ -27,7 +27,7 @@ export const codeMirrorOptions = {
 
   // 高级配置（需要引入对应的插件包）,codemirror advanced options(You need to manually introduce the corresponding codemirror function script code)
   // sublime、emacs、vim三种键位模式，支持你的不同操作习惯
-  keyMap: 'sublime',
+  // keyMap: 'sublime',
   // 按键映射，比如Ctrl键映射autocomplete，autocomplete是hint代码提示事件
   extraKeys: { Ctrl: 'autocomplete' },
   lineWrapping: true,
