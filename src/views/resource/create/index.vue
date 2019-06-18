2c81e78b4e750b7af0e73afc1c435534109fb32b
kai <kai@kaideMacBook-Pro.local>
2019-06-18 15:36:11 +0800

    Update index.vue

--- src/views/resource/create/index.vue
+++ src/views/resource/create/index.vue
@@ -2,40 +2,40 @@
     <section class="create-resource-wrapper">
         <div class="bd">
             <resource-input
-                    :data="resourceDetail"
-                    ref="inputArea"
+                :data="resourceDetail"
+                ref="inputArea"
             >
                 <div style="height: 60px;"></div>
                 <div
-                        class="btm-wrap clearfix"
-                        style="position: fixed; bottom: 0; left: 0; right: 0; height: 60px; margin: 0; display: flex; justify-content: center; background-color: #fff; box-shadow: 0 -2px 5px 0 rgba(0,0,0,0.1); z-index: 100;"
+                    class="btm-wrap clearfix"
+                    style="position: fixed; bottom: 0; left: 0; right: 0; height: 60px; margin: 0; display: flex; justify-content: center; background-color: #fff; box-shadow: 0 -2px 5px 0 rgba(0,0,0,0.1); z-index: 100;"
                 >
                     <div
-                            class="rt-side"
-                            style="width: 1190px; height: 100%; display: flex; align-items: center; justify-content: flex-end;"
+                        class="rt-side"
+                        style="width: 1190px; height: 100%; display: flex; align-items: center; justify-content: flex-end;"
                     >
                         <el-button
-                                class="ft-btn"
-                                style="color: #999999;"
-                                @click="cancelHandler"
-                                type="text"
+                            class="ft-btn"
+                            style="color: #999999;"
+                            @click="cancelHandler"
+                            type="text"
                         >{{
                             $t('common.cancelCreateText') }}
                         </el-button>
                         <el-button
-                                class="ft-btn"
-                                round
-                                @click="create2AddHandler"
+                            class="ft-btn"
+                            round
+                            @click="create2AddHandler"
                         >
-<!--                            {{$t('resource.createDoneText')}}-->
+                            <!--                            {{$t('resource.createDoneText')}}-->
                             {{isResourceIdEditMode ? '保存': '完成创建'}}
                         </el-button>
                         <el-button
-                                class="ft-btn"
-                                style="margin-left: 30px;"
-                                type="primary"
-                                round
-                                @click="create2AddHandler"
+                            class="ft-btn"
+                            style="margin-left: 30px;"
+                            type="primary"
+                            round
+                            @click="create2AddHandler"
                         >
                             <!--                            {{$t('resource.createDoneText')}}-->
                             {{isResourceIdEditMode ? '保存': '创建'}}并发行
