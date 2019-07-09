722293e94864956256f20bcb212376cd7a4f639c
kai <kai@kaideMacBook-Pro.local>
2019-07-09 14:09:48 +0800

    Update mock.js

--- src/router/mock.js
+++ src/router/mock.js
@@ -27,44 +27,6 @@ export default {
             },
             component: Views.mockDisplay
         },
-        // {
-        //     path: 're-edit',
-        //     meta: {
-        //         requiresAuth: true,
-        //         // title: i18n.t('routes.resourcePolicyTplList'),
-        //         title: i18n.t('routes.resourcePolicyTplList'),
-        //         type: 'resource',
-        //         hideFooter: true,
-        //         theme: 'gray'
-        //     },
-        //     component: Views.mockReEdit
-        // },
-        // {
-        //     path: 'create',
-        //     hidden: true,
-        //     meta: {
-        //         requiresAuth: true,
-        //         // title: i18n.t('routes.createResource'),
-        //         title: '创建模拟资源',
-        //         type: 'resource',
-        //         theme: 'gray',
-        //         hideFooter: true,
-        //     },
-        //     component: Views.mockCreator,
-        // },
-        // {
-        //     path: 'create/:mockResourceId',
-        //     hidden: true,
-        //     meta: {
-        //         requiresAuth: true,
-        //         // title: i18n.t('routes.createResource'),
-        //         title: '模拟资源管理',
-        //         type: 'resource',
-        //         theme: 'gray',
-        //         hideFooter: true,
-        //     },
-        //     component: Views.mockCreator,
-        // },
         {
             path: 'create/:bucketName',
             hidden: true,
@@ -83,80 +45,11 @@ export default {
             meta: {
                 requiresAuth: true,
                 // title: i18n.t('routes.createResource'),
-                title: '创建模拟资源',
+                title: '模拟资源管理',
                 theme: 'gray',
                 hideFooter: true,
             },
             component: Views.mockEditor,
         },
-        // {
-        //     path: 'edit/:resourceId',
-        //     hidden: true,
-        //     meta: {
-        //         requiresAuth: true,
-        //         title: i18n.t('routes.updateResource'),
-        //         type: 'resource',
-        //         theme: 'gray',
-        //         // hideSidebar: true
-        //     },
-        //     component: Views.mockEditor,
-        // },
-        // {
-        //     path: 'list',
-        //     meta: {
-        //         requiresAuth: true,
-        //         title: i18n.t('routes.myResources'),
-        //         type: 'resource'
-        //     },
-        //     component: Views.mockList
-        // },
-        // {
-        //     path: 'detail',
-        //     hidden: true,
-        //     redirect: '/resource/list',
-        // },
-        // {
-        //     path: 'detail/:resourceId',
-        //     hidden: true,
-        //     meta: {
-        //         requiresAuth: true,
-        //         title: i18n.t('routes.resourceDetail'),
-        //         type: 'resource',
-        //         theme: 'gray'
-        //     },
-        //     component: Views.mockDetail
-        // },
-        // {
-        //     path: 'policy_tpl',
-        //     hidden: true,
-        //     meta: {
-        //         requiresAuth: true,
-        //         type: 'resource'
-        //     },
-        //     component: Views.container,
-        //     redirect: '/resource/policy_tpl/list',
-        //     children: [
-        //         {
-        //             path: 'create',
-        //             hidden: true,
-        //             meta: {
-        //                 requiresAuth: true,
-        //                 title: i18n.t('routes.createResourcePolicyTpl'),
-        //                 type: 'resource'
-        //             },
-        //             component: Views.policyTplCreator
-        //         },
-        //         {
-        //             path: 'detail',
-        //             hidden: true,
-        //             meta: {
-        //                 requiresAuth: true,
-        //                 title: i18n.t('routes.resourcePolicyTplDetail'),
-        //                 type: 'resource'
-        //             },
-        //             component: Views.policyTplDetail
-        //         }
-        //     ]
-        // }
     ]
 }
